36369f866bd7ea0bc770669ce65343ab65818568
Dhruv Chavda <2025.dhruvc@isu.ac.in>
2026-02-17 09:54:32 +0530

    Refactor random number generation and add restart functionality

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 
-        let a = Math.random() * 100;
-        a = Number.parseInt(a);
+let a = Math.floor(Math.random() * 100);
 
-        let input;
-        let score = 100;
+let input;
+let score = 100;
 
-        const inputBox = document.getElementById("guessInput");
-        const button = document.getElementById("guessBtn");
-        const message = document.querySelector(".message");
-        const scoreSpan = document.getElementById("score");
+const inputBox = document.getElementById("guessInput");
+const button = document.getElementById("guessBtn");
+const message = document.querySelector(".message");
+const scoreSpan = document.getElementById("score");
+const restartBtn = document.getElementById("restartBtn");
 
-        button.addEventListener("click", function () {
+button.addEventListener("click", function () {
 
-            input = inputBox.value;
-            score = score - 1;
-            scoreSpan.innerText = score;
+    input = inputBox.value;
+    score = score - 1;
+    scoreSpan.innerText = score;
 
-            if (input == a) {
-                message.innerText = "Congratulations! You have guessed the correct number.";
-                button.disabled = true;
-                inputBox.disabled = true;
-            }
-            else if (input > a && input < 100) {
-                message.innerText = "Your guess is too high";
-            }
-            else if (input < a && input > 0) {
-                message.innerText = "Your guess is too low";
-            }
-            else {
-                message.innerText = "Please enter a valid number between 0 and 100";
-            }
+    if (input == a) {
+        message.innerText = "Congratulations! You have guessed the correct number.";
+        inputBox.disabled = true;
 
-            inputBox.value = "";
-        });
+    button.style.display = "none";       
+    restartBtn.style.display = "inline-block"; 
+    }
+    else if (input > a && input < 100) {
+        message.innerText = "Your guess is too high";
+    }
+    else if (input < a && input > 0) {
+        message.innerText = "Your guess is too low";
+    }
+    else {
+        message.innerText = "Please enter a valid number between 0 to 100";
+    }
+
+    inputBox.value = "";
+});
+restartBtn.addEventListener("click", function () {
+
+    a = Math.floor(Math.random() * 100);
+    score = 100;
+
+    scoreSpan.innerText = score;
+    message.innerText = "Game restarted! Guess a new number.";
+    inputBox.disabled = false;
+    inputBox.value = "";
+
+    restartBtn.style.display = "none"; 
+    button.style.display = "inline-block";
+});
+
